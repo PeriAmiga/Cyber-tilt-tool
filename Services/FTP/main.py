490c73bd6cdc1fe8f765f53fdb73ebe2ad26a5f0
Peri Amiga <periamiga13@gmail.com>
2023-01-15 03:53:26 +0200

    ftp

---
+++ Services/FTP/main.py
@@ -0,0 +1,45 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+
+# The port the FTP server will listen on.
+# This must be greater than 1023 unless you run this script as root.
+FTP_PORT = 21
+
+# The name of the FTP user that can log in.
+FTP_USER = "root"
+
+# The FTP user's password.
+FTP_PASSWORD = "toor"
+
+# The directory the FTP user will have full read/write access to.
+FTP_DIRECTORY = "FTPServer"
+
+
+def main():
+    authorizer = DummyAuthorizer()
+
+    # Define a new user having full r/w permissions.
+    authorizer.add_user(FTP_USER, FTP_PASSWORD, FTP_DIRECTORY, perm='elradfmw')
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "Client Connected Successfully."
+
+    # Optionally specify range of ports to use for passive connections.
+    #handler.passive_ports = range(60000, 65535)
+
+    address = ('169.254.109.213', FTP_PORT)
+    server = FTPServer(address, handler)
+
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
