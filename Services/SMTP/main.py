6190ea4edcf459fd5fb98623f4242a1a505ea783
Peri Amiga <periamiga13@gmail.com>
2023-03-04 20:48:59 +0200

    Update main.py

--- Services/SMTP/main.py
+++ Services/SMTP/main.py
@@ -1,28 +1,20 @@
 import asyncio
 from aiosmtpd.controller import Controller
+from aiosmtpd.handlers import Debugging
 
-class CustomSMTPServer:
-    async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
-        envelope.rcpt_tos.append(address)
-        return '250 OK'
+class CustomController(Controller):
+    def factory(self):
+        return Debugging(self)
 
-    async def handle_DATA(self, server, session, envelope):
-        print('Received message from %s' % envelope.mail_from)
-        print('Recipients:', envelope.rcpt_tos)
-        print('Message data:\n%s' % envelope.content.decode('utf8', errors='replace'))
-        return '250 Message accepted for delivery'
-
-async def main(loop):
-    handler = CustomSMTPServer()
-    controller = Controller(handler, loop=loop, hostname='10.0.0.9', port=25)
-    controller.start()
+async def start_server(loop):
+    controller = CustomController(
+        hostname='10.0.0.9',
+        port=1035,
+        loop=loop
+    )
+    await controller.start()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(main(loop))
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        loop.stop()
+    loop.create_task(start_server(loop))
+    loop.run_forever()
